03fd44814a4fa02f347f92d80a51a8acab2c4d5f
Nguyễn Thành Tỷ <22004071@st.vlute.edu.vn>
2026-06-29 09:44:56 +0700

    seed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\BookController;
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/seed', function () {
+    Artisan::call('migrate --force');
+    Artisan::call('db:seed --force');
+    return 'Seed done';
+});
+
 Route::get('/', function () {
 return redirect()->route('books.index');
 });
